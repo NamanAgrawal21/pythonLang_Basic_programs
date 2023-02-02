d170434465a5865f8d5a087db047d3384f1b8807
NamanAgrawal21 <117016403+NamanAgrawal21@users.noreply.github.com>
2023-02-03 00:55:30 +0530

    Add files via upload

---
+++ Basic Programming Exercises/01_area_of_circle.py
@@ -0,0 +1,9 @@
+# Program to print the diameter,circumference and area of circle.
+
+rad=float(input("Enter the radius of the circle:"))
+dia=2*rad
+circum=2*3.14*rad
+area=3.14*rad**2
+print("Diameter of the given circle is",dia)
+print("Circumference of the given circle is",circum)
+print("Area of the given circle is",area)
---
+++ Basic Programming Exercises/02_area_of_rectangle.py
@@ -0,0 +1,8 @@
+# Program to print the perimeter and area of rectangle.
+
+l=float(input("Enter the length of the rectangle:"))
+b=float(input("Enter the breadth of the rectangle:"))
+peri=2*(l+b)
+area=l*b
+print("Perimeter of the given rectangle is",peri)
+print("Area of the given rectangle is",area)
